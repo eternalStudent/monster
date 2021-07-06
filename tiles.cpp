4828b1b87632318f773415e221f2fdfb852041eb
eternalStudent <noam.weisberg@gmail.com>
2021-07-06 20:32:42 +0300

    rename sprites.cpp to tiles.cpp

---
+++ tiles.cpp
@@ -0,0 +1,66 @@
+struct Sprite {
+	union {
+		TextureHandle spritesheet;
+		TextureHandle tileset;
+		TextureHandle texture;
+	};
+	Box2 crop;
+};
+
+typedef Sprite Brush;
+static Brush blackBrush;
+
+Brush GenerateBrush(uint32 rgba) {
+	Sprite brush;
+	brush.texture = GenerateTextureFromRGBA(rgba);
+	brush.crop = BOX2_UNIT();
+	return brush;
+}
+
+
+// Tiles  
+//-----------------------             
+
+struct Tile {
+	int32 index;
+	union {
+		Sprite sprite;
+		struct {TextureHandle tileset; Box2 crop;};
+	};
+	uint32 flags; // 1 - blocking
+};
+
+static Tile tiles[1024] = {};
+
+#define W_ 62
+#define H_ 34
+
+// TODO: either these should be computed, or the tile-size
+// probably should be moved
+#define X_ 31
+#define Y_ 17
+
+static int32 grid[W_][H_] = {};
+
+void SpritesInit() {
+	TextureHandle tileset = GenerateTextureFromFile("grass_tileset.bmp", Pixelated);
+	for (int32 i=0; i<16; i++){
+		int32 x = i / 4;
+		int32 y = i % 4;
+		Tile* tile = &tiles[i+1];
+		tile->tileset = tileset;
+		tile->crop = {x*0.25f, y*0.25f, (x+1)*0.25f, (y+1)*0.25f};
+		tile->flags = 1;
+	}
+
+	tileset = GenerateTextureFromFile("stuff_tileset.bmp", Pixelated);
+	for (int32 i=0; i<16; i++){
+		int32 x = i / 4;
+		int32 y = i % 4;
+		Tile* tile = &tiles[i+17];
+		tile->tileset = tileset;
+		tile->crop = {x*0.25f, y*0.25f, (x+1)*0.25f, (y+1)*0.25f};
+	}
+
+	blackBrush = GenerateBrush(0xff000000);
+}
